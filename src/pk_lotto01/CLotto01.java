3f11ade7893113218b49eabfaac53910c4607370
xxx <xxx@web.de>
2022-11-14 21:21:31 +0100

    master-LottoStatistics methods implemented

--- src/pk_lotto01/CLotto01.java
+++ src/pk_lotto01/CLotto01.java
@@ -1,12 +1,74 @@
 package pk_lotto01;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+
+class Statistic implements Comparable<Statistic>{
+    private int number = 0;
+    private int occurrence = 0;
+
+    public Statistic(){}
+    public Statistic(int number, int occurrence) {
+        this.number = number;
+        this.occurrence = occurrence;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public int getOccurrence() {
+        return occurrence;
+    }
+    public void setOccurrence(int occurrence) {
+        this.occurrence = occurrence;
+    }
+    @Override
+    public String toString() {
+        return "Statistic{" +
+                "number=" + number +
+                ", occurrence=" + occurrence +
+                '}';
+    }
+    @Override
+    public int compareTo(Statistic o) {
+        if (this.getOccurrence() != o.getOccurrence()) {
+            return o.getOccurrence() - this.getOccurrence();
+        }
+        return 0;
+    }
+}
 
 public class CLotto01 {
-    public static void doOneLottoDrawing_Simplified(){
+    public static Statistic[] lottoStatistics = new Statistic[49];
+
+    public static void displayLottoStatistics(){
+        Arrays.sort(lottoStatistics);
+        Statistic elem;
+        for(int i = 0; i < lottoStatistics.length; i++){
+            elem = lottoStatistics[i];
+            if(elem != null){
+                System.out.printf("%02d times drawn: %02d %n", elem.getOccurrence(), elem.getNumber());
+            }
+        }
+    }
+    public static void initializeLottoStatisticsArray(){
+        for(int i = 0; i < 49; i++){
+            Statistic obj = new Statistic();
+            obj.setNumber((i+1));
+            obj.setOccurrence(0);
+            lottoStatistics[i] = obj;
+        }
+    }
+    public static void setStatisticsForOneLottoDrawing(List<Integer> li){
+        if(li.isEmpty()) throw new RuntimeException("empty list error");
+        for(Integer elem : li){
+            int index = elem-1;
+            Statistic obj = lottoStatistics[index];
+            obj.setOccurrence(obj.getOccurrence()+1);
+        }
+    }
+    public static List<Integer> doOneLottoDrawing_Simplified(){
         List<Integer> lottoNumbersList = new ArrayList<>();
         for(int i = 0; i < 49; i++){
             lottoNumbersList.add(i+1); //1-49
@@ -25,11 +87,17 @@ public class CLotto01 {
         //Comparator<Integer> comp = (number1, number2) -> {return number1.compareTo(number2);};
         Comparator<Integer> comp = Comparator.naturalOrder();
         lottoDrawSixList.sort(comp);
-        System.out.println("Lotto Numbers drawn: " + lottoDrawSixList);
+        return lottoDrawSixList;
     }
     public static void main(String[] args) {
-        for(int i = 0; i < 20; i++){
-            doOneLottoDrawing_Simplified();
+
+        initializeLottoStatisticsArray();
+        System.out.println(Arrays.asList(lottoStatistics));
+
+        for(int i = 0; i < 100; i++){
+            List<Integer> li = doOneLottoDrawing_Simplified();
+            setStatisticsForOneLottoDrawing(li);
         }
+        displayLottoStatistics();
     }
 }
